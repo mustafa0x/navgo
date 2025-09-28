8c09184f8649e56117422b30cfb7948c5ab0cd1d
Mustafa J <mustafa.0x@gmail.com>
2025-09-28 09:26:29 +0300

    minor refactors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import { parse } from 'regexparam'
 
 export default function Navaid(base, on404) {
-	var rgx, curr, routes=[], $={};
+	let curr
+	let $ = {}
+	const routes = []
+	const normalize = uri => '/' + (uri || '').replace(/^\/|\/$/g, '')
 
-	var fmt = $.format = function (uri) {
-		if (!uri) return uri;
-		uri = '/' + uri.replace(/^\/|\/$/g, '');
-		return rgx.test(uri) && uri.replace(rgx, '/');
+	base = normalize(base)
+	const rgx = base == '/' ? /^\/+/ : new RegExp('^\\' + base + '(?=\\/|$)\\/?', 'i')
+
+	$.format = function (uri) {
+		if (!uri) return uri
+		uri = normalize(uri)
+		return rgx.test(uri) && uri.replace(rgx, '/')
 	}
 
-	base = '/' + (base || '').replace(/^\/|\/$/g, '');
-	rgx = base == '/' ? /^\/+/ : new RegExp('^\\' + base + '(?=\\/|$)\\/?', 'i');
-
 	$.route = function (uri, replace) {
-		if (uri[0] == '/' && !rgx.test(uri)) uri = base + uri;
-		history[(uri === curr || replace ? 'replace' : 'push') + 'State'](uri, null, uri);
+		if (uri[0] == '/' && !rgx.test(uri)) uri = base + uri
+		history[(uri === curr || replace ? 'replace' : 'push') + 'State'](uri, null, uri)
 	}
 
 	$.on = function (pat, fn) {
@@ -25,67 +28,72 @@ export default function Navaid(base, on404) {
 	}
 
 	$.run = function (uri) {
-		var i=0, params={}, arr, obj;
-		if (uri = fmt(uri || location.pathname)) {
-			uri = uri.match(/[^\?#]*/)[0];
+		let i = 0
+		const params = {}
+		let arr
+		let obj
+		if ((uri = $.format(uri || location.pathname))) {
+			uri = uri.match(/[^\?#]*/)[0]
 			for (curr = uri; i < routes.length; i++) {
-				if (arr = (obj=routes[i]).pattern.exec(uri)) {
-					for (i=0; i < obj.keys.length;) {
-						params[obj.keys[i]] = arr[++i] || null;
+				if ((arr = (obj = routes[i]).pattern.exec(uri))) {
+					for (i = 0; i < obj.keys.length; ) {
+						params[obj.keys[i]] = arr[++i] || null
 					}
-					obj.fn(params); // todo loop?
-					return $;
+					obj.fn(params) // todo loop?
+					return $
 				}
 			}
-			if (on404) on404(uri);
+			on404?.(uri)
 		}
-		return $;
+		return $
 	}
 
 	$.listen = function (u) {
-		wrap('push');
-		wrap('replace');
+		wrap('push')
+		wrap('replace')
 
 		function run(e) {
-			$.run();
+			$.run()
 		}
 
 		function click(e) {
-			var x = e.target.closest('a'), y = x && x.getAttribute('href');
-			if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.button || e.defaultPrevented) return;
-			if (!y || x.target || x.host !== location.host || y[0] == '#') return;
-			if (y[0] != '/' || rgx.test(y)) {
-				e.preventDefault();
-				$.route(y);
+			const el = e.target.closest('a')
+			const href = el?.getAttribute('href')
+			if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.button || e.defaultPrevented)
+				return
+			if (!href || el.target || el.download || el.host !== location.host || href[0] == '#') return
+			if (href[0] != '/' || rgx.test(href)) {
+				e.preventDefault()
+				$.route(href)
 			}
 		}
 
-		addEventListener('popstate', run);
-		addEventListener('replacestate', run);
-		addEventListener('pushstate', run);
-		addEventListener('click', click);
+		addEventListener('popstate', run)
+		addEventListener('replacestate', run)
+		addEventListener('pushstate', run)
+		addEventListener('click', click)
 
 		$.unlisten = function () {
-			removeEventListener('popstate', run);
-			removeEventListener('replacestate', run);
-			removeEventListener('pushstate', run);
-			removeEventListener('click', click);
+			removeEventListener('popstate', run)
+			removeEventListener('replacestate', run)
+			removeEventListener('pushstate', run)
+			removeEventListener('click', click)
 		}
 
-		return $.run(u);
+		return $.run(u)
 	}
 
-	return $;
+	return $
 }
 
 function wrap(type, fn) {
-	if (history[type]) return;
-	history[type] = type;
-	fn = history[type += 'State'];
+	if (history[type]) return
+	history[type] = type
+	fn = history[(type += 'State')]
 	history[type] = function (uri) {
-		var ev = new Event(type.toLowerCase());
-		ev.uri = uri;
-		fn.apply(this, arguments);
-		return dispatchEvent(ev);
+		let ev = new Event(type.toLowerCase())
+		ev.uri = uri
+		fn.apply(this, arguments)
+		return dispatchEvent(ev)
 	}
 }
